b0d604b9c9027733293f70f2afab3c5aa46b9eec
Ariane Camilo <arianecamilo.alves@gmail.com>
2018-09-14 01:03:47 -0300

    uploading files

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        Vue.js Calculator
+        <div class="autoria">
+            Por Ariane Camilo
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container{
+    width: 400px;
+    margin: 0 auto;
+    font-family: 'Courier New', Courier, monospace;
+    font-weight: bold;
+    font-size: 40px;
+    color:darkgreen;
+    padding: 20px;
+}
+.autoria{
+    font-size: 15px;
+    font-family: 'Lucida Sans', Arial, sans-serif;
+    color: #333;
+}
+</style>
